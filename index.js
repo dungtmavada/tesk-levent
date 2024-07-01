7669406f2081e67e45b8875620c29927eabd371c
MDung <minhdungit2001@gmail.com>
2024-07-01 13:55:25 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.post('/', function (req, res) {
+    console.log(req.body)
+    res.status(200).json({app: true});
+})
+
+
+
+
+
+
+app.listen(4444, function () {
+    console.log('Example app listening on port 4444!');
+});
